52bfcb85943881a31d8a08b13c647faec6fc605e
ItzShekhawat <48554422+ItzShekhawat@users.noreply.github.com>
2020-03-29 23:56:30 +0200

    Aggiunta dei predecessori di ogni nodo

--- dijkstra_shekhawat (2).py
+++ dijkstra_shekhawat (2).py
@@ -1,10 +1,12 @@
-grafo = {'a':{'b':1,'c':4},'b':{'c':1, 'd':3},'c':{'d':1},'d':{}}  #dizionario del grafo con una slite modifica
+grafo = {'a':{'b':1,'c':4},'b':{'c':1, 'd':3},'c':{'d':1},'d':{}}  #dizionario del grafo with a slite modification 
+
 #grafo_prova = {0:{1:1, 2:4}, 1:{2:1 , 3:3}, 2:{3:1}, 3:{}}
 
 def dijkstra(grafo,start):
     shot_path = {} # Contiene il numero minimo di costo per raggiungere un certo nodo 
     grafoNodes = grafo #Conterrà il grafo per poi sapere quante volte esegure una certa cosa
     infinity = 100000 #Un numero abbastanza grande da non essere superato
+    predecessori = {}
 
 
     for node in grafoNodes: #Inizializzo tutti i nodi a infinito tranne il primo
@@ -22,10 +24,16 @@ def dijkstra(grafo,start):
         for childNode, weight in grafo[minNode].items():                # Faccio un ciclo per ogni elmento di ogni blocco della dello shot_path
             if weight + shot_path[minNode] < shot_path[childNode]:      # controllo per vedere se il peso + l'etichetta nel mio nodo sono minori di quelli sucessivi(figli) 
                 shot_path[childNode] = weight + shot_path[minNode]      # se vero, rietichetto il nodo figlio con un valore minore
+                predecessori[childNode] = minNode                       # contengo la predecessori di ogni nodo
+        
         grafoNodes.pop(minNode)    # mi permette di uscire dal while
     
-   
+    print("Label min : ")
     print(shot_path)
+    print("Predecessori : ")
+    print(predecessori)
+    
+    input("Qualsiasi tasto per finire. ")
 
 
 if __name__ == "__main__":
